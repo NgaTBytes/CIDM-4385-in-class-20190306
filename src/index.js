646832918ed4d690341b21b3180b314a4eae24bd
Nga Nguyen <jadegrl806@gmail.com>
2019-03-06 14:06:46 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'jquery/dist/jquery'
+import 'popper.js/dist/popper'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
