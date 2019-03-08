7df031fbb748128ec13a118ce6c7d73824c26e24
Becca Gerlich <becca_gerlich@Beccas-MacBook-Pro-3.local>
2019-03-07 21:33:25 -0600

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
-
 import LoginForm from './Components/LoginForm';
-
-
+import PizzaPlaces from './Components/PizzaPlaces';
 import './App.css';
 
+console.log(process.env.REACT_APP_MAPBOX_API_ACCESS_TOKEN);
+console.log(process.env.NODE_ENV);
+
 const Map = ReactMapboxGl({
-  accessToken: 'pk.eyJ1IjoiYWh1aW1hbnU2OSIsImEiOiJjanNpYnZnNG0wbTB6NDlxb3VqbzQ0ZjRpIn0.a4bjWgujDq3W3RxGVeuwVw',
-}); 
+  accessToken: "pk.eyJ1IjoiamNtdXNpayIsImEiOiJjanNvNWFuaWswajkzNDVwOXQyejQwbGQzIn0.xxNLUG46Y-7QspUsLShJrA"
+});
 
 class App extends Component {
 
@@ -22,11 +23,12 @@ class App extends Component {
       lat: 39.828175,
       zoom: 2,
       mapstyle: mapstyles[Math.floor(Math.random() * mapstyles.length)],
+      pizza_place_list: [],
     };
-
+    this.getPizzaPlacesFromHereAPI = this.getPizzaPlacesFromHereAPI.bind(this);
     this.handleFormSubmission = this.handleFormSubmission.bind(this);
     this.randomizeMapStyle = this.randomizeMapStyle.bind(this);
-
+    this.setCurrentLocation = this.setCurrentLocation.bind(this);
   }
 
   randomizeMapStyle(){
@@ -36,6 +38,85 @@ class App extends Component {
 
   //lifecycle method
   componentDidMount(){
+    this.setCurrentLocation();
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot){
+
+    /*so we don't update on every little change, just check to see
+      if lat or lon changed */
+    if(this.state.lat !== prevState.lat || this.state.lng !== prevState.lng){
+
+      console.log(`Previous Lat: ${prevState.lat} and Prevous Lon:${prevState.lng}`);
+      console.log(`Current Lat: ${this.state.lat} and Current Lon:${this.state.lng}`);      
+
+      //make rest call
+      this.getPizzaPlacesFromHereAPI();    
+    }
+
+  }
+
+  getPizzaPlacesFromHereAPI(){
+    
+    const here_api_url      = "https://places.cit.api.here.com/places/v1/autosuggest?";
+    const here_api_at       = `at=${this.state.lat},${this.state.lng}&`;
+    const here_api_q        = "q=pizza&";
+    const here_api_app_id   = `app_id=${process.env.REACT_APP_HERE_API_APP_ID}&`;
+    const here_api_app_code = `app_code=${process.env.REACT_APP_HERE_API_APP_CODE}`;
+
+    //the built-in fetch API will make the REST/AJAX call for us: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch    
+    fetch(here_api_url + here_api_at + here_api_q + here_api_app_id + here_api_app_code)
+      .then( (response) => {
+          //call HERE API and get returned list
+          return response.json();
+        }
+      )
+      //filter down to response that have locations (lat/lon)
+      .then( (responseAsJson) => {
+
+        //use the JavaScript filter method - https://www.w3schools.com/jsref/jsref_filter.asp
+        const filtered = responseAsJson.results.filter( (result) => {
+          //this checks to see if this record has a position array
+          return result.position;
+
+        });
+
+        //return the filtered results
+        return filtered;
+        
+      })
+      //receive the promise response returned as a JSON object
+      .then( (filtered) => {
+
+          this.setState( () => {
+            return {
+              pizza_place_list: filtered,
+            }
+          }
+        );
+
+        return filtered;
+      })
+      .then( (filtered) => {
+
+        this.state.pizza_place_list.forEach( (pizza_place) => {
+            const pizzalocation = pizza_place.title + ' ' +
+                                  pizza_place.vicinity + ' ' +
+                                  pizza_place.category;
+
+            console.log(pizzalocation);
+          }
+        );
+        console.log(this.state.pizza_place_list[2].title);
+      })
+      .catch(error => console.error(error));
+}
+
+  handleFormSubmission(formdata){
+
+  }
+
+  setCurrentLocation(){
     //check to see if we can get the browser's geolocation
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition(position => {
@@ -51,11 +132,7 @@ class App extends Component {
     }else{
       console.log("Geolocation is not supported by this browser.");
     }
-  }
-
-  handleFormSubmission(formdata){
-
-  }
+}
 
   render() {
 
@@ -64,12 +141,12 @@ class App extends Component {
 
     return (
       <div className="container">
-        <LoginForm onFormSubmit={this.handleFormSubmission} />
+        <LoginForm onFormSubmit={this.handleFormSubmission} title="Cheap Pizza"/>
         <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
         <Map style={`mapbox://styles/mapbox/${mapstyle}-v9`}
              center={[lng, lat]}
              containerStyle={{
-              height: "400px",
+              height: window.screen.availHeight / 3 + "px",
               width: "100%"
              }}>
              <Layer type="symbol"
@@ -78,6 +155,7 @@ class App extends Component {
                     <Feature coordinates={[lng, lat]}/>
              </Layer>
         </Map>
+        <PizzaPlaces places={this.state.pizza_place_list} />
       </div>
     );
   }
--- src/Components/PasswordInput.js
+++ src/Components/PasswordInput.js
@@ -27,7 +27,6 @@ class PasswordInput extends Component {
                 }
             }
         );
-
     }
 
     render() {
